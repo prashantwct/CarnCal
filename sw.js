1a846c0e25100582117f0f16a006a3aa86ac60ca
prashantwct <prasarsenal@gmail.com>
2025-12-14 16:00:55 +0000

    offline

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'carncal-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event: cache the files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch event: serve from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
