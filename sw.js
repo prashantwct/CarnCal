877ea8031e47a7c72ea80adbc021f48951962084
prashantwct <prasarsenal@gmail.com>
2025-12-16 08:01:16 +0530

    Add additional assets to service worker cache
    Added new assets for caching in service worker.

--- sw.js
+++ sw.js
@@ -2,13 +2,20 @@ const CACHE_NAME = 'carncal-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './app.js', // Functional Improvement
+  './icons/cc-72.png',
+  './icons/cc-96.png',
+  './icons/cc-144.png',
+  './icons/cc-192.png',
+  './icons/cc-512.png'
 ];
 
 // Install event: cache the files
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Note: If you place your icons in a separate folder, ensure the paths are correct.
       return cache.addAll(ASSETS);
     })
   );
